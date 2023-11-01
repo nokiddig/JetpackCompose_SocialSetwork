16ef1aa516724daa1e6a1db54a27a82050e01374
HP <sy2000dn0@gmail.com>
2023-11-01 10:02:50 +0700

    Test Text

--- app/src/main/java/com/example/setup_jetpack_compose/MainActivity.kt
+++ app/src/main/java/com/example/setup_jetpack_compose/MainActivity.kt
@@ -3,13 +3,26 @@ package com.example.setup_jetpack_compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.setup_jetpack_compose.ui.theme.Setup_jetpack_composeTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +35,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Demo()
                 }
             }
         }
@@ -30,17 +43,34 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Demo() {
+    Column {
+        TextHello()
+        Spacer(modifier = Modifier.height(12.dp))
+        TextHello()
+    }
 }
 
-@Preview(showBackground = true)
+@Composable
+fun TextHello() {
+    Text(
+        text = stringResource(id = R.string.test_text),
+        color = Color.Red ,
+        fontSize = 30.sp,
+        fontStyle = FontStyle.Italic,
+        fontWeight = FontWeight(500),
+        textAlign = TextAlign.Start,
+        fontFamily = FontFamily.Cursive,
+        textDecoration = TextDecoration.LineThrough,
+        maxLines = 2,
+        overflow = TextOverflow.Ellipsis
+        )
+}
+
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun GreetingPreview() {
     Setup_jetpack_composeTheme {
-        Greeting("Android")
+        Demo()
     }
 }
