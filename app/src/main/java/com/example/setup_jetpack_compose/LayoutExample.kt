cf4adb902d80723ae3ef48958dc43e90ff65aa8c
HP <sy2000dn0@gmail.com>
2023-11-02 16:12:28 +0700

    save code - radio button

--- app/src/main/java/com/example/setup_jetpack_compose/LayoutExample.kt
+++ app/src/main/java/com/example/setup_jetpack_compose/LayoutExample.kt
@@ -6,13 +6,21 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.RadioButton
+import androidx.compose.material3.RadioButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
@@ -24,19 +32,31 @@ import androidx.compose.ui.unit.sp
 @Preview (showSystemUi = true)
 @Composable
 fun LayoutDemo() {
-    BoxDemo()
+    ColumnDemo()
 }
 
 @Composable
 fun ColumnDemo() {
+    val radioOptions = listOf<String>("op 1", "op 2", "op 3")
+    val selectedOption = remember { mutableStateOf(radioOptions[0]) }
+
     Column {
-        Text("Xin chào", fontSize = 20.sp)
-        Text("Xin chào", fontSize = 20.sp)
-        Text("Xin chào", fontSize = 20.sp)
-        Text("Xin chào", fontSize = 20.sp)
-        Row {
-            Text("Xin chào", fontSize = 20.sp)
-            Text("Xin chào", fontSize = 20.sp)
+        LazyColumnDemo()
+        radioOptions.forEach { option ->
+            Row(
+                Modifier.padding(vertical = 4.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                RadioButton(
+                    selected = (option == selectedOption.value),
+                    onClick = { selectedOption.value = option },
+                    colors = RadioButtonDefaults.colors(selectedColor = Color.Red)
+                )
+                Text(
+                    text = option,
+                    modifier = Modifier.padding(start = 8.dp)
+                )
+            }
         }
     }
 }
@@ -84,6 +104,20 @@ fun BoxDemo() {
     }
 }
 
+@Composable
+fun ListPreview() {
+    
+}
 
-class LayoutExample {
+@Composable
+fun LazyColumnDemo() {
+    LazyColumn (
+        modifier = Modifier
+            .size(height = 20.dp, width = 100.dp)
+            .fillMaxSize()
+    ){
+        items(5){
+            Text("Line $it")
+        }
+    }
 }
